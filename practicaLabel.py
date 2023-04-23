a3de2b50ef82c1a065fa910a6941cb9de7157f94
vceniceros <vceniceros2001@gmail.com>
2023-04-22 21:29:02 -0300

    cambios en el ejercicio

--- practicaLabel.py
+++ practicaLabel.py
@@ -6,6 +6,28 @@ miFrame = Frame(root, width=500, height=400)
 
 miFrame.pack()
 
-miLabel = Label(miFrame, text="Esto es bocaaa",fg="yellow", bg="blue",font=("Comic Sans MS",18)).place(x=100,y=200)
+miNombre = StringVar()
 
+cuadro_de_nombre = Entry(miFrame, textvariable=miNombre)
+cuadro_de_nombre.grid(row=0,column=1,padx=10,pady=10)
+
+nombreLable= Label(miFrame,text="Nombre:")
+nombreLable.grid(row=0,column=0,sticky="e",padx=10,pady=10)
+
+cuadro_de_apellido = Entry(miFrame)
+cuadro_de_apellido.grid(row=1,column=1,padx=10,pady=10)
+
+apellidoLable= Label(miFrame,text="Apellido:")
+apellidoLable.grid(row=1,column=0, sticky="e",padx=10,pady=10)
+
+cuadro_de_direccion = Entry(miFrame)
+cuadro_de_direccion.grid(row=2,column=1,padx=10,pady=10)
+
+direccionLable= Label(miFrame,text="Direccion:")
+direccionLable.grid(row=2,column=0,sticky="e",padx=10,pady=10)
+def sendButton():
+    miNombre.set("valentino")
+
+boton = Button(root, text="enviar", command=sendButton)
+boton.pack()
 root.mainloop()
