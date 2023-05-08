c76c59bb8fddef6ccc497b04e50ad03a6eb37b80
vceniceros <85922025+vceniceros@users.noreply.github.com>
2023-05-08 16:14:30 -0300

    final de la parte 5

--- parte5.py
+++ parte5.py
@@ -1,66 +1,32 @@
-import doctest
 
-def Puntaje(resultado):
+def acumulate_points(results,SUCCESS_POINTS = 10, FAIL_POINTS = -3):
     """
-    esta funcion recibira un booleano dependiendo del acierto o desacierto del jugador,
-    si acierta sera verdadero y si le erra sera falso, de esto dependera la suma o el resto de puntajes
+    recibe una tupla con aciertos y errores y devuelve el puntaje total del juego como entero
     """
-    suma_punto = 0
-    if resultado:
-        suma_punto += 10
-    else:
-        suma_punto-=3
-    return suma_punto
-def Bucle():
-    """
-    en esta funcion se establece un bucle donde el usuario indica si quiere seguir jugando
-    si aprieta s se sigue sino se corta
-    """
-    puntos = 0
-    decision = 's'
-    try:
-         while decision != 'n':
-        
-          decision = input("¿desea seguir jugando?(s para si, n para no): ")
-          prueba_res = input("t o f: ")
-          if prueba_res == 't':
-              prueba_res = True
-          else:
-              prueba_res = False
-          puntos += Puntaje(prueba_res)
-    except TypeError:
-             print("error de tipado, por favor ingrese s para si o n para no")
-             decision = input("¿desea seguir jugando?(s para si, n para no): ")
-    return print(puntos)
+    SUCCESS_INDEX = 0
+    FAIL_INDEX = 1
+    success_point = results[SUCCESS_INDEX]*SUCCESS_POINTS
+    fail_point = results[FAIL_INDEX]*FAIL_POINTS
+    return success_point+fail_point
 
-def PuntajeTupla(aciertos,errores,ACIERTOS = 10, ERRORES = -3):
+def play_match():
     """
-    esta funcion es la que calcula los puntajes por tupla
+    le pregunta al jugador si desea seguir jugando y devuelve el puntaje final
     """
-    puntos_acierto = aciertos*ACIERTOS
-    puntos_errores = errores*ERRORES
-    return puntos_acierto+puntos_errores
-def BucleTupla():
-    """
-    estes es el bucle por tupla
-    """
-    puntos = 0
-    decision = 's'
-    sumatoria_aciertos = 0
-    sumatoria_errores = 0
-    try:
-         while decision != 'n':
-        
-          decision = input("¿desea seguir jugando?(s para si, n para no): ")
-          prueba_res = input("t o f: ")
-          if prueba_res == 't':
-              sumatoria_aciertos +=1
-          else:
-              sumatoria_errores +=1
-          puntos = PuntajeTupla(sumatoria_aciertos,sumatoria_errores)
-    except TypeError:
-             print("error de tipado, por favor ingrese s para si o n para no")
-             decision = input("¿desea seguir jugando?(s para si, n para no): ")
-    return print(puntos)
+    points = 0
+    decision = True 
+    while decision:
+        #TODO: result = aca va la funcion de lucas
+        points += acumulate_points(results)
+        print(f"Puntaje Final: {points}")
+        play_again = str(input("si desea seguir jugando, escriba ok: "))
+        if play_again != 'ok':
+            decision = False
+    
 
-BucleTupla()
+def main():
+    """ 
+    ejecuta el juego
+    """
+    play_match()
+main()
