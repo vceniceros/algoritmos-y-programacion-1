4bb6d36bf2eb6730b0e3fd2a5a6f40b89c7acd45
vceniceros <vceniceros2001@gmail.com>
2023-05-08 09:01:53 -0300

    parte 5 primeros pasitos de bb

--- parte5.py
+++ parte5.py
@@ -1,4 +1,6 @@
-def puntaje(resultado):
+
+
+def Puntaje(resultado):
     """
     esta funcion recibira un booleano dependiendo del acierto o desacierto del jugador,
     si acierta sera verdadero y si le erra sera falso, de esto dependera la suma o el resto de puntajes
@@ -9,3 +11,28 @@ def puntaje(resultado):
     else:
         suma_punto-=3
     return suma_punto
+def Bucle():
+    """
+    en esta funcion se establece un bucle donde el usuario indica si quiere seguir jugando
+    si aprieta s se sigue sino se corta
+    """
+    puntos = 0
+    decision = 's'
+    try:
+         while decision != 'n':
+        
+          decision = input("¿desea seguir jugando?(s para si, n para no): ")
+          prueba_res = input("t o f: ")
+          if prueba_res == 't':
+              prueba_res = True
+          else:
+              prueba_res = False
+          puntos += Puntaje(prueba_res)
+    except TypeError:
+             print("error de tipado, por favor ingrese s para si o n para no")
+             decision = input("¿desea seguir jugando?(s para si, n para no): ")
+    return print(puntos)
+
+Bucle()
+
+
