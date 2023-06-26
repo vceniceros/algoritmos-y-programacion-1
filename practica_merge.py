c06598f4176e377471daa40834eda831fbc08976
vceniceros <vceniceros2001@gmail.com>
2023-06-26 11:03:51 -0300

    mas practica de merge

--- practica_merge.py
+++ practica_merge.py
@@ -118,10 +118,105 @@ def mating_archive(parcial1):
         write_mating_archive(parcial1, str(padron), str(nota_final))
         padron, nota_1, nota_2, nota_3, nota_4 = read_Lines(correctores)
 #merge mezcla, apareo actualiza en base al merge
+
+
+
 mating_archive(parcial1)        
 correctores.close()
 parcial1.close()
 
+#------------------------------------------------------------------------------------------
+#ejercicio 3: todo lo que sigue es para perfeccionar lo anterior
+END_ARCHIVE_PARCIAl = MAX_PADRON+','
+END_ARCHIVE_CURSO = MAX_PADRON+',,,'
 
 
+def read_lines_parcial(archive):
+    line = archive.readline()
+    return line.rstrip('\n').split(',') if line else END_ARCHIVE_PARCIAl.split(',') 
 
+def read_lines_curso(archive):
+    line = archive.readline()
+    return line.rstrip('\n').split(',') if line else END_ARCHIVE_CURSO.split(',') 
+
+def write_curso_parcial(archive, padron, nombre, apellido, nota):
+    archive.write(padron + ',' + nombre + ',' + apellido + ','+ nota + '\n')
+
+def open_parcial1_curso():
+    try:
+        parcial1_curso = open('7540_1C2020_parcial1.csv', 'r+')
+    except:
+        parcial1_curso = open('7540_1C2020_parcial1.csv', 'w')
+        parcial1_curso.close()
+        parcial1_curso = open('7540_1C2020_parcial1.csv', 'r+')
+    return parcial1_curso
+
+curso = open('7540_1C2020.csv', 'r+')
+parcial1 = open('parcial_1.csv', 'r+')
+parcial1_curso = open_parcial1_curso()
+
+
+def mateing_parcial1_curso(parcial1_curso, curso, parcial1):
+
+    padron_parcial, nota = read_lines_parcial(parcial1)
+    padron_curso, nombre, apellido, correo = read_lines_curso(curso)
+
+    while int(padron_parcial) < int(MAX_PADRON) or int(padron_curso) < int(MAX_PADRON):
+        while padron_parcial == padron_curso < MAX_PADRON:
+            padron = padron_parcial
+            write_curso_parcial(parcial1_curso, padron, nombre, apellido, nota)
+            padron_parcial, nota = read_lines_parcial(parcial1)
+            padron_curso, nombre, apellido, correo = read_lines_curso(curso)
+
+mateing_parcial1_curso(parcial1_curso, curso, parcial1)
+curso.seek(0)
+parcial1.close()
+
+
+def write_curso_parcial_aprobado(archive ,correo, nombre, nota):
+    archive.write('Para: ' + correo + 'Asunto: Aprobaste el parcial de Algoritmos I! :)' + ' Contenido: hola' + nombre + '! Aprobaste el parcial de Algoritmos I con un ' + nota + '\n')
+
+def write_curso_parcial_desaprobado(archive ,correo, nombre, nota):
+    archive.write('Para: ' + correo + 'Asunto: Reprobaste el parcial de Algoritmos I! :(' + ' Contenido: hola' + nombre + '! Reprobaste el parcial de Algoritmos I con un ' + nota + ' .El recuperatorio es el jueves 2/7. Saludos!' + '\n')
+
+def open_aprobados():
+    try:
+        aprobados = open('aprobados.txt', 'r+')
+    except:
+        aprobados = open('aprobados.txt', 'w')
+        aprobados.close()
+        aprobados = open('aprobados.txt', 'r+') 
+    return aprobados
+
+def open_reprobados():
+    try:
+        reprobados = open('reprobados.txt', 'r+')
+    except:
+        reprobados = open('reprobados.txt', 'w')
+        reprobados.close()
+        reprobados = open('reprobados.txt', 'r+') 
+    return reprobados
+
+aprobados = open_aprobados()
+reprobados = open_reprobados()
+
+def apareo_curso_parcial_aprobados(aprobados, reprobados, parcial1_curso, curso):
+    padron_parcial, nombre, apellido, nota = read_lines_curso(parcial1_curso)
+    padron_curso, nombre, apellido, correo = read_lines_curso(curso)
+
+    while int(padron_parcial) < int(MAX_PADRON) or int(padron_curso) < int(MAX_PADRON):
+        while padron_parcial == padron_curso < MAX_PADRON and float(nota) >= 4.0:
+            write_curso_parcial_aprobado(aprobados, correo, nombre, nota)
+            padron_parcial, nombre, apellido, nota = read_lines_curso(parcial1_curso)
+            padron_curso, nombre, apellido, correo = read_lines_curso(curso)
+        while padron_parcial == padron_curso < MAX_PADRON and float(nota) < 4.0:
+            write_curso_parcial_desaprobado(reprobados, correo, nombre, nota)
+            padron_parcial, nombre, apellido, nota = read_lines_curso(parcial1_curso)
+            padron_curso, nombre, apellido, correo = read_lines_curso(curso)
+
+apareo_curso_parcial_aprobados(aprobados, reprobados, parcial1_curso, curso)
+
+aprobados.close()
+reprobados.close()
+curso.close()
+parcial1_curso.close()
